9b5cb19867e171bbde82b818f4d93f3c177bdc45
abdelilah1223 <151017371+abdelilah1223@users.noreply.github.com>
2025-05-11 03:28:02 +0100

    Update app.js

--- public/app.js
+++ public/app.js
@@ -6,7 +6,10 @@ const socket = io('https://live-production-cf6e.up.railway.app', {
     path: '/socket.io/',
     reconnection: true,
     reconnectionAttempts: 5,
-    reconnectionDelay: 1000
+    reconnectionDelay: 1000,
+    timeout: 20000,
+    forceNew: true,
+    autoConnect: true
 });
 
 // Initialize PeerJS
